706203a23c2cc484ea556c561acf4c5197b47a17
johnytiago <joaosao10@gmail.com>
2017-03-04 13:54:06 +0000

    Added DB and manager classes; Creates DB and Record table now

---
+++ src/java/controller/Manager.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class Manager {
+	/*
+    public void register(Key publicKey) {}
+
+    public void put(Key publicKey, byte[] domain, byte[] username, byte[] password){}
+
+    public String get(Key publicKey, byte[] domain, byte[] username){
+    	return password;
+    }
+    */
+}
