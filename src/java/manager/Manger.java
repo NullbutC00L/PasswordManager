6c3f17fb0e63efeed4bab18e5d91d85330ac2aee
snackk <santos_matrix@sapo.pt>
2017-03-07 14:29:52 +0000

    All manager methods throws PMException.

--- src/java/manager/Manger.java
+++ src/java/manager/Manger.java
@@ -2,6 +2,8 @@ package manager;
 
 import java.util.ArrayList;
 import domain.DomainCredentials;
+import exception.PasswordManagerExceptionHandler;
+
 import java.util.HashMap;
 
 public class Manger {
@@ -9,22 +11,22 @@ public class Manger {
 	private HashMap<byte[], HashMap<byte[], ArrayList<DomainCredentials>>> _pubKeys = new HashMap<byte[], HashMap<byte[], ArrayList<DomainCredentials>>>();
 	
 	/*TODO - implement*/
-	public byte[] searchPassword(byte[] pubKey, byte[] domain, byte[] username){
+	public byte[] searchPassword(byte[] pubKey, byte[] domain, byte[] username) throws PasswordManagerExceptionHandler{
 		return null;
 	} 
 	
 	/*TODO - implement*/
-	public void delete(byte[] pubKey, byte[] domain, byte[] username){
+	public void delete(byte[] pubKey, byte[] domain, byte[] username) throws PasswordManagerExceptionHandler{
 		
 	}
 	
 	/*TODO - implement*/
-	public void insert(byte[] pubKey, byte[] domain, byte[] username){
+	public void insert(byte[] pubKey, byte[] domain, byte[] username) throws PasswordManagerExceptionHandler{
 		
 	} 
 	
 	/*TODO - implement*/
-	public void register(byte[] pubKey, byte[] domain, byte[] username){
+	public void register(byte[] pubKey, byte[] domain, byte[] username) throws PasswordManagerExceptionHandler{
 		
 	} 
 }
