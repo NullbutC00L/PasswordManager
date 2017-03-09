51e07349ec3fa2d9845a7310065a0478c131d6f3
snackk <santos_matrix@sapo.pt>
2017-03-09 18:39:28 +0000

    Fixed binding error from uncomplete connection.

--- src/java/ws/PasswordManagerWSPublisher.java
+++ src/java/ws/PasswordManagerWSPublisher.java
@@ -4,9 +4,20 @@ import javax.xml.ws.Endpoint;
 
 import message.PasswordManagerMessages;
 
+@SuppressWarnings("restriction")
 public class PasswordManagerWSPublisher {
 	public static void main(String[] args) {
-		Endpoint.publish(PasswordManagerMessages.PASSWORD_MANAGER_ADRESS, new PasswordManagerWSImpl());
+		final Endpoint ep = Endpoint.create(new PasswordManagerWSImpl());
+		ep.publish(PasswordManagerMessages.PASSWORD_MANAGER_ADRESS);
+		
 		System.out.println("Published on: " + PasswordManagerMessages.PASSWORD_MANAGER_ADRESS);
+		
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+		    public void run() {
+		    	ep.stop();
+		    	System.out.println("WS shutdown.");
+		    }
+		 });
+		
 	}
 }
