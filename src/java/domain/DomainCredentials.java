4161c65b76686315297a5e3b95e06066fa11b9fa
johnytiago <joaosao10@gmail.com>
2017-04-04 20:07:18 +0100

    Domaincredentials stores the tripletHash

--- src/java/domain/DomainCredentials.java
+++ src/java/domain/DomainCredentials.java
@@ -3,10 +3,12 @@ package domain;
 public class DomainCredentials {
 	private byte[] _username;
 	private byte[] _password;
+	private byte[] _tripleHash;
 	
-	public DomainCredentials(byte[] username, byte[] password){
+	public DomainCredentials(byte[] username, byte[] password, byte[] tripletHash){
 		_username = username;
 		_password = password;
+		_tripleHash = tripletHash;
 	}
 	
 	public byte[] getUsername(){
@@ -15,5 +17,9 @@ public class DomainCredentials {
 	
 	public byte[] getPassword(){
 		return _password;
+	}
+
+	public byte[] getTripletHash(){
+		return _tripleHash;
 	}
 }
