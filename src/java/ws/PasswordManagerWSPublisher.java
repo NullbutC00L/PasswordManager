77a74678b22675fb72c20cb7ff4a309a438218fe
johnytiago <joaosao10@gmail.com>
2017-04-29 18:18:05 +0100

    SecretKeyStored moved to common

--- src/java/ws/PasswordManagerWSPublisher.java
+++ src/java/ws/PasswordManagerWSPublisher.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import message.PasswordManagerMessages;
 import crypto.Crypto;
 
 public class PasswordManagerWSPublisher {
