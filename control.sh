0aa16a8153a95ca56ef7435848dfe4a51bc64436
johnytiago <joaosao10@gmail.com>
2017-05-01 12:13:19 +0100

    Improved control script

--- control.sh
+++ control.sh
@@ -5,30 +5,42 @@ OIFS="$IFS"; IFS=$'\n'; PIDS=($(<pids.txt)); IFS="$OIFS"
 # Prevent error assigning variable
 if [ $2 != "all" ]
 then
-  PROCESS=${PIDS[((--$2))]}
+  PROCESS=${PIDS[(($(($2-1))))]}
 fi 
 
 case $1 in
   resume )
     kill -SIGCONT $PROCESS
     echo $PROCESS resumed
+      echo -e "\033[1;32mRESUMED\033[0m $PROCESS" 
     ;;
   stop )
     if [ $2 == "all" ]
     then
       kill -SIGTERM ${PIDS[*]} >& /dev/null
-      echo Stopped all replicas
+      echo -e "\033[1;32mSTOPPED\033[0m all replicas" 
     else
       kill -SIGTERM $PROCESS
-      echo $PROCESS stopped
+      echo -e "\033[1;32mSTOPPED\033[0m $PROCESS" 
     fi
     ;;
   pause )
     kill -SIGSTOP $PROCESS
-    echo $PROCESS paused
+    echo -e "\033[1;32mPAUSED\033[0m $PROCESS" 
     ;;
   status )
-    ps 28288 -o state,pid,time
+    if [ $2 == "all" ]
+    then
+      for PROCESS in $PIDS; do
+        echo -e "\033[1;32mSTATUS\033[0m for $PROCESS"
+        ps $PROCESS -o state,pid,time
+        lsof -Pan -p $PROCESS -i
+      done
+    else
+      echo -e "\033[1;32mSTATUS\033[0m for $PROCESS" 
+      ps $PROCESS -o state,pid,time 
+      lsof -Pan -p $PROCESS -i
+    fi 
     ;;
   * )
     echo Command $1 not found.
