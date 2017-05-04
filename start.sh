ae8de64b802e09dc4be1005e5857bf61e35b5ee9
skigth <fmjgomes@gmail.com>
2017-05-04 11:29:32 +0100

    sh update

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-NUM_REPLICAS=${NUM_REPLICAS:=2}
+NUM_REPLICAS=${NUM_REPLICAS:=6}
 PORT=${PORT:=8080}
-FAULT = $fault
+NUM_FAULTS = ${NUM_FAULTS=3}
 
 # function that calculates the number of N processes based on the number of f
 # faulty servers
@@ -30,11 +30,10 @@ mkdir keys &> /dev/null
 rm -rf keys/* &> /dev/null
 
 FINAL_PORT=$(getN)
-read -p "whats the max fault number: " fault
 for CURRENT_PORT in $(seq $PORT $FINAL_PORT)
 do
   LOG=/tmp/$CURRENT_PORT.log 
-  PORT=$CURRENT_PORT mvn exec:java > $LOG & 
+  PORT=$CURRENT_PORT NUM_FAULTS = $NUM_FAULTS mvn exec:java > $LOG & 
   # Store pid for futher control (e.g pause, stop)
   echo $! >> pids.txt
   echo -e "\033[1;32mSUCCESS\033[0m" started replica running on port $CURRENT_PORT. Process PID=$!.
