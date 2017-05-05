fd4931dd45752ed2bb2b98536cd2d700364dad85
johnytiago <joaosao10@gmail.com>
2017-05-05 09:17:46 +0100

    Fix identation

--- src/java/layer/Security.java
+++ src/java/layer/Security.java
@@ -11,49 +11,49 @@ import envelope.Envelope;
 import util.SecretKeyStore;
 
 public class Security {
-	private Crypto crypto;
-	private util.Util util = new util.Util();
-	private SecretKeyStore dhKeyStore = new SecretKeyStore();
+  private Crypto crypto;
+  private util.Util util = new util.Util();
+  private SecretKeyStore dhKeyStore = new SecretKeyStore();
 
-	
-	public Security (Crypto crypto){
-		this.crypto = crypto;
-	}
-	private boolean verifyHMAC( Envelope envelope) {
-	    SecretKey DHSecretKey = dhKeyStore.get( envelope.getDHPublicKey() );
-	    byte[] HMAC = crypto.genMac( util.msgToByteArray( envelope.getMessage() ), DHSecretKey );
-	    return Arrays.equals(HMAC, envelope.getHMAC());
-	  }
 
-	  private void addHMAC( Envelope envelope, byte[] DHPubKeyCli ) {
-	    SecretKey DHSecretKey = dhKeyStore.get( DHPubKeyCli );
-	    byte[] HMAC = crypto.genMac( util.msgToByteArray(envelope.getMessage()), DHSecretKey );
-	    envelope.setHMAC( HMAC );
-	    return;
-	  }
+  public Security (Crypto crypto){
+    this.crypto = crypto;
+  }
+  private boolean verifyHMAC( Envelope envelope) {
+    SecretKey DHSecretKey = dhKeyStore.get( envelope.getDHPublicKey() );
+    byte[] HMAC = crypto.genMac( util.msgToByteArray( envelope.getMessage() ), DHSecretKey );
+    return Arrays.equals(HMAC, envelope.getHMAC());
+  }
 
-	  public void prepareEnvelope( Envelope envelope, byte[] DHPubKeyCli ) {
-	    envelope.getMessage().setCounter( crypto.addCounter(DHPubKeyCli) );
-	    envelope.getMessage().setPublicKey( crypto.getPublicKey().getEncoded() ); 
-	    envelope.setDHPublicKey( crypto.getDHPublicKey().getEncoded() );
-	    byte[] msg = util.singnableByteArray(envelope.getMessage());
-	    envelope.setSignature(crypto.genSign( msg , (PrivateKey)crypto.getPrivateKey() ));
-	    // Must be the last to add to envelope
-	    addHMAC( envelope, DHPubKeyCli );
-	    return;
-	  }
+  private void addHMAC( Envelope envelope, byte[] DHPubKeyCli ) {
+    SecretKey DHSecretKey = dhKeyStore.get( DHPubKeyCli );
+    byte[] HMAC = crypto.genMac( util.msgToByteArray(envelope.getMessage()), DHSecretKey );
+    envelope.setHMAC( HMAC );
+    return;
+  }
 
-	  public boolean verifyEnvelope( Envelope envelope ) {
-	    generateDH( envelope );
-	    byte[] msg = util.singnableByteArray( envelope.getMessage() );
-	    Boolean sign = crypto.verSign(msg, crypto.retrievePubKey(envelope.getMessage().publicKey), envelope.getSignature());
-	    return verifyHMAC( envelope ) && sign && crypto.verifyCounter( envelope.getDHPublicKey(), envelope.getMessage().counter);
-	  }
+  public void prepareEnvelope( Envelope envelope, byte[] DHPubKeyCli ) {
+    envelope.getMessage().setCounter( crypto.addCounter(DHPubKeyCli) );
+    envelope.getMessage().setPublicKey( crypto.getPublicKey().getEncoded() ); 
+    envelope.setDHPublicKey( crypto.getDHPublicKey().getEncoded() );
+    byte[] msg = util.singnableByteArray(envelope.getMessage());
+    envelope.setSignature(crypto.genSign( msg , (PrivateKey)crypto.getPrivateKey() ));
+    // Must be the last to add to envelope
+    addHMAC( envelope, DHPubKeyCli );
+    return;
+  }
 
-	  private void generateDH( Envelope envelope){
-	    PublicKey DHPubKeyCli = crypto.retrieveDHPubKey( envelope.getDHPublicKey() );
-	    SecretKey DHSecretKey = crypto.generateDH( crypto.getDHPrivateKey(), DHPubKeyCli );
-	    // Store key to save CPU
-	    dhKeyStore.put( envelope.getDHPublicKey(), DHSecretKey);
-	  }
+  public boolean verifyEnvelope( Envelope envelope ) {
+    generateDH( envelope );
+    byte[] msg = util.singnableByteArray( envelope.getMessage() );
+    Boolean sign = crypto.verSign(msg, crypto.retrievePubKey(envelope.getMessage().publicKey), envelope.getSignature());
+    return verifyHMAC( envelope ) && sign && crypto.verifyCounter( envelope.getDHPublicKey(), envelope.getMessage().counter);
+  }
+
+  private void generateDH( Envelope envelope){
+    PublicKey DHPubKeyCli = crypto.retrieveDHPubKey( envelope.getDHPublicKey() );
+    SecretKey DHSecretKey = crypto.generateDH( crypto.getDHPrivateKey(), DHPubKeyCli );
+    // Store key to save CPU
+    dhKeyStore.put( envelope.getDHPublicKey(), DHSecretKey);
+  }
 }
--- src/java/layer/SecurityWSClient.java
+++ src/java/layer/SecurityWSClient.java
@@ -15,106 +15,106 @@ import util.SecretKeyStore;
 import ws.client.*;
 
 public class SecurityWSClient {
-	private Crypto _crypto;
-	private SecretKeyStore dhKeyStore = new SecretKeyStore();
-	private boolean DEBUG = Boolean.valueOf((System.getenv("DEBUG")));
+  private Crypto _crypto;
+  private SecretKeyStore dhKeyStore = new SecretKeyStore();
+  private boolean DEBUG = Boolean.valueOf((System.getenv("DEBUG")));
 
-	public SecurityWSClient (Crypto crypto){
-		this._crypto = crypto;
-	}
+  public SecurityWSClient (Crypto crypto){
+    this._crypto = crypto;
+  }
 
-	public byte[] addSalt(byte[] data, byte[] salt){
-		try{
-			ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
-			outputStream.write( data );
-			outputStream.write( salt );
-			return outputStream.toByteArray();
-		} catch (Exception e){
-			e.printStackTrace();
-			return null;
-		}
-	}
+  public byte[] addSalt(byte[] data, byte[] salt){
+    try{
+      ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
+      outputStream.write( data );
+      outputStream.write( salt );
+      return outputStream.toByteArray();
+    } catch (Exception e){
+      e.printStackTrace();
+      return null;
+    }
+  }
 
-	private boolean verifyHMAC( Envelope envelope ) {
-		SecretKey DHSecretKey = dhKeyStore.get( envelope.getDHPublicKey() );
-		byte[] HMAC = _crypto.genMac( msgToByteArray( envelope.getMessage() ), DHSecretKey );
-		return Arrays.equals(HMAC, envelope.getHMAC());
-	}
+  private boolean verifyHMAC( Envelope envelope ) {
+    SecretKey DHSecretKey = dhKeyStore.get( envelope.getDHPublicKey() );
+    byte[] HMAC = _crypto.genMac( msgToByteArray( envelope.getMessage() ), DHSecretKey );
+    return Arrays.equals(HMAC, envelope.getHMAC());
+  }
 
-	private void addHMAC( Envelope envelope, byte[] dhPubKeySrv ) {
-		SecretKey DHSecretKey = dhKeyStore.get( dhPubKeySrv );
-		byte[] HMAC = _crypto.genMac( msgToByteArray( envelope.getMessage() ), DHSecretKey );
-		envelope.setHMAC( HMAC );
-		return;
-	}
+  private void addHMAC( Envelope envelope, byte[] dhPubKeySrv ) {
+    SecretKey DHSecretKey = dhKeyStore.get( dhPubKeySrv );
+    byte[] HMAC = _crypto.genMac( msgToByteArray( envelope.getMessage() ), DHSecretKey );
+    envelope.setHMAC( HMAC );
+    return;
+  }
 
-	public void prepareEnvelope( Envelope envelope, byte[] pubKeySrv ) {
-		generateDH( envelope, pubKeySrv );
-		envelope.getMessage().setPublicKey( _crypto.getPublicKey().getEncoded() ); 
-		envelope.setDHPublicKey( _crypto.getDHPublicKey().getEncoded() );
-		envelope.getMessage().setCounter(_crypto.addCounter(pubKeySrv));
-	    byte[] msg = singnableByteArray(envelope.getMessage());
-	    envelope.setSignature(_crypto.genSign( msg , (PrivateKey)_crypto.getPrivateKey() ));
-		// Must be the last to add to envelope
-		addHMAC( envelope, pubKeySrv );
-		return;
-	}
+  public void prepareEnvelope( Envelope envelope, byte[] pubKeySrv ) {
+    generateDH( envelope, pubKeySrv );
+    envelope.getMessage().setPublicKey( _crypto.getPublicKey().getEncoded() ); 
+    envelope.setDHPublicKey( _crypto.getDHPublicKey().getEncoded() );
+    envelope.getMessage().setCounter(_crypto.addCounter(pubKeySrv));
+    byte[] msg = singnableByteArray(envelope.getMessage());
+    envelope.setSignature(_crypto.genSign( msg , (PrivateKey)_crypto.getPrivateKey() ));
+    // Must be the last to add to envelope
+    addHMAC( envelope, pubKeySrv );
+    return;
+  }
 
-	public boolean verifyEnvelope( Envelope envelope ) {
-		byte[] msg = singnableByteArray( envelope.getMessage() );
-		Boolean sign = _crypto.verSign(msg, _crypto.retrievePubKey(envelope.getMessage().getPublicKey()), envelope.getSignature());
-		
-		if( DEBUG ) {
-			System.out.println("[DEBUG] MAC verification passed? " + verifyHMAC( envelope ));
-			System.out.println("[DEBUG] Counter verification passed? " + _crypto.verifyCounter( envelope.getDHPublicKey(), envelope.getMessage().getCounter() ));
-			System.out.println("[DEBUG] Sign verification passed? " + sign);
-		}
-		return verifyHMAC( envelope ) && sign && _crypto.verifyCounter( envelope.getDHPublicKey(), envelope.getMessage().getCounter() );
-	}
+  public boolean verifyEnvelope( Envelope envelope ) {
+    byte[] msg = singnableByteArray( envelope.getMessage() );
+    Boolean sign = _crypto.verSign(msg, _crypto.retrievePubKey(envelope.getMessage().getPublicKey()), envelope.getSignature());
 
-	private void generateDH( Envelope envelope, byte[] pubKeySrv ){
-		PublicKey DHPubKeySrv = _crypto.retrieveDHPubKey( pubKeySrv );
-		SecretKey DHSecretKey = _crypto.generateDH( _crypto.getDHPrivateKey(), DHPubKeySrv );
-		// Store key to save CPU
-		dhKeyStore.put( pubKeySrv , DHSecretKey);
-	}	
-	
-	private byte[] msgToByteArray(Message msg) {
-	    ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
-	    try {
-	      if ( msg.getPublicKey() != null)
-	        outputStream.write( msg.getPublicKey() );
-	      if ( msg.getDomainHash() != null)
-	          outputStream.write( msg.getDomainHash() );
-	      if ( msg.getUsernameHash() != null)
-	        outputStream.write( msg.getUsernameHash() );
-	      if ( msg.getPassword() != null)
-	        outputStream.write( msg.getPassword() );
-	      if ( msg.getTripletHash() != null)
-	        outputStream.write( msg.getTripletHash() );
-	      outputStream.write( msg.getWts() );
-	      outputStream.write( msg.getRid() );
-	      outputStream.write( msg.getCounter() );
-	      return outputStream.toByteArray();
-	    } catch( Exception e) {
-	      e.printStackTrace();
-	      return null;
-	    }
-	}
-	
-	public byte[] singnableByteArray(Message msg) {
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
-		try {
-			if ( msg.getPassword() != null)
-				outputStream.write( msg.getPassword() );
-			if ( msg.getTripletHash() != null)
-				outputStream.write( msg.getTripletHash() );
-			outputStream.write( msg.getWts() );
-			return outputStream.toByteArray();
-		} catch( Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+    if( DEBUG ) {
+      System.out.println("[DEBUG] MAC verification passed? " + verifyHMAC( envelope ));
+      System.out.println("[DEBUG] Counter verification passed? " + _crypto.verifyCounter( envelope.getDHPublicKey(), envelope.getMessage().getCounter() ));
+      System.out.println("[DEBUG] Sign verification passed? " + sign);
+    }
+    return verifyHMAC( envelope ) && sign && _crypto.verifyCounter( envelope.getDHPublicKey(), envelope.getMessage().getCounter() );
+  }
+
+  private void generateDH( Envelope envelope, byte[] pubKeySrv ){
+    PublicKey DHPubKeySrv = _crypto.retrieveDHPubKey( pubKeySrv );
+    SecretKey DHSecretKey = _crypto.generateDH( _crypto.getDHPrivateKey(), DHPubKeySrv );
+    // Store key to save CPU
+    dhKeyStore.put( pubKeySrv , DHSecretKey);
+  }	
+
+  private byte[] msgToByteArray(Message msg) {
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
+    try {
+      if ( msg.getPublicKey() != null)
+        outputStream.write( msg.getPublicKey() );
+      if ( msg.getDomainHash() != null)
+        outputStream.write( msg.getDomainHash() );
+      if ( msg.getUsernameHash() != null)
+        outputStream.write( msg.getUsernameHash() );
+      if ( msg.getPassword() != null)
+        outputStream.write( msg.getPassword() );
+      if ( msg.getTripletHash() != null)
+        outputStream.write( msg.getTripletHash() );
+      outputStream.write( msg.getWts() );
+      outputStream.write( msg.getRid() );
+      outputStream.write( msg.getCounter() );
+      return outputStream.toByteArray();
+    } catch( Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  public byte[] singnableByteArray(Message msg) {
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
+    try {
+      if ( msg.getPassword() != null)
+        outputStream.write( msg.getPassword() );
+      if ( msg.getTripletHash() != null)
+        outputStream.write( msg.getTripletHash() );
+      outputStream.write( msg.getWts() );
+      return outputStream.toByteArray();
+    } catch( Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
 }
 
